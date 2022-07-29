e077f5543edcbc17a7823c886e9952e9c7ade873
Anna Vidutina <a.vidutina@yandex.ru>
2022-07-29 20:43:10 +0500

    адаптив 320 main

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,7 +4,7 @@ import { NavMovie } from './components/NavMovie/NavMovie';
 
 export const Navigation = () => {
   // Данный useState временный, для выполнения этапа верстка
-  const [login, setLogin] = useState(true);
+  const [login, setLogin] = useState(false);
 
   return <>{login ? <NavMovie /> : <NavAuth />}</>;
 };
