414928bce138e7eb0463c4af73135d6f5a49b285
Anna Vidutina <a.vidutina@yandex.ru>
2022-07-29 19:39:11 +0500

    верстка и JSX скрытое меню

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,7 +4,7 @@ import { NavMovie } from './components/NavMovie/NavMovie';
 
 export const Navigation = () => {
   // Данный useState временный, для выполнения этапа верстка
-  const [login, setLogin] = useState(false);
+  const [login, setLogin] = useState(true);
 
   return <>{login ? <NavMovie /> : <NavAuth />}</>;
 };
--- src/components/Navigation/components/NavMovie/NavMovie.js
+++ src/components/Navigation/components/NavMovie/NavMovie.js
@@ -1,35 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './NavMovie.css';
+import { HiddenMenu } from './HiddenMenu/HiddenMenu';
 
 export const NavMovie = () => {
-  return (
-    <nav className='nav-movie__menu'>
-      <div className='nav-movie__movie'>
-        <NavLink
-          to='/movies'
-          className={({ isActive }) =>
-            isActive ? 'nav-movie__link active' : 'nav-movie__link'
-          }
-        >
-          Фильмы
-        </NavLink>
-        <NavLink
-          to='/saved-movies'
-          className={({ isActive }) =>
-            isActive ? 'nav-movie__link active' : 'nav-movie__link'
-          }
-        >
-          Сохранённые фильмы
-        </NavLink>
-      </div>
+  const [toggle, setToggle] = useState(true);
 
-      <NavLink
-        to='/profile'
-        className='nav-movie__link nav-movie__link_profile'
-      >
-        Аккаунт
-      </NavLink>
-    </nav>
+  const onClickOpenMenu = () => {
+    setToggle(false);
+  };
+
+  const onClickCloseMenu = () => {
+    setToggle(true);
+  };
+
+  return (
+    <>
+      {toggle ? (
+        <div className='nav-menu__menu-adapt' onClick={onClickOpenMenu}></div>
+      ) : (
+        <HiddenMenu onClickCloseMenu={onClickCloseMenu} />
+      )}
+      <nav className='nav-movie__menu'>
+        <div className='nav-movie__movie'>
+          <NavLink
+            to='/movies'
+            className={({ isActive }) =>
+              isActive ? 'nav-movie__link active' : 'nav-movie__link'
+            }
+          >
+            Фильмы
+          </NavLink>
+          <NavLink
+            to='/saved-movies'
+            className={({ isActive }) =>
+              isActive ? 'nav-movie__link active' : 'nav-movie__link'
+            }
+          >
+            Сохранённые фильмы
+          </NavLink>
+        </div>
+
+        <NavLink
+          to='/profile'
+          className='nav-movie__link nav-movie__link_profile'
+        >
+          Аккаунт
+        </NavLink>
+      </nav>
+    </>
   );
 };
